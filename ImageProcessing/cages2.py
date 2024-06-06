bff52c0b743a795ee595b8655ac397138f80cfd0
thearhansheth <arhan.sheth@gmail.com>
2024-06-06 16:05:39 +0530

    Added inverse and otsu thresholds to add black cages

--- ImageProcessing/cages2.py
+++ ImageProcessing/cages2.py
@@ -1,12 +1,12 @@
 import numpy as np
 import cv2 as cv
  
-img = cv.imread("/Users/arhan.sheth/Documents/Codes/DX/Basics/cages.png")
+img = cv.imread("/Users/arhan.sheth/Documents/Codes/DX/Basics/dx_opencvInitial/ImageProcessing/cages.png")
 assert img is not None, "file could not be read, check with os.path.exists()"
 ogImage = img.copy()
 imgray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
 ret, thresh = cv.threshold(imgray, 140, 255, 0)
-ret2, thresh2 = cv.threshold(imgray, 65, 255, 0)
+ret2, thresh2 = cv.threshold(imgray, 140, 255, cv.THRESH_BINARY_INV + cv.THRESH_OTSU)
 contours, hierarchy = cv.findContours(thresh, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 contours2, hierarchy2 = cv.findContours(thresh2, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
 
