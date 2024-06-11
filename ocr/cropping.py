dea2fbbd9ba18cbb75a958d46c74dd0821d74879
thearhansheth <arhan.sheth@gmail.com>
2024-06-11 11:50:44 +0530

    Cropped iamges and stores individual results

--- ocr/cropping.py
+++ ocr/cropping.py
@@ -3,3 +3,15 @@ import numpy as np
 
 img = cv.imread("/Users/arhan.sheth/Documents/Codes/DX/Basics/dx_opencvInitial/arhan_ocr_task/images/sample input images/cropImg.jpeg")
 grayimg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
+print(grayimg.shape)
+
+img1 = grayimg[130:1080, 110:800]
+img2 = grayimg[130:1050, 800:1380]
+
+cv.imwrite("image_1.png", img1)
+cv.imwrite("image_2.png", img2)
+cv.imshow("Image 1", img1)
+cv.imshow("Image 2", img2)
+cv.imshow("Original", img)
+cv.waitKey(0)
+cv.destroyAllWindows()
