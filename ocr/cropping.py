220f9f245a68484e625a442da14092b8dd282081
thearhansheth <arhan.sheth@gmail.com>
2024-06-11 12:19:14 +0530

    minor change to import lines

--- ocr/cropping.py
+++ ocr/cropping.py
@@ -1,5 +1,4 @@
 import cv2 as cv
-import numpy as np
 
 img = cv.imread("/Users/arhan.sheth/Documents/Codes/DX/Basics/dx_opencvInitial/arhan_ocr_task/images/sample input images/cropImg.jpeg")
 grayimg = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
