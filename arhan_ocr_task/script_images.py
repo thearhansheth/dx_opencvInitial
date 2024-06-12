a3e4e8d3a8bae8cdaa5402a735545201bf39ad48
thearhansheth <arhan.sheth@gmail.com>
2024-06-12 12:41:27 +0530

    Added guj and san traineddata models

--- arhan_ocr_task/script_images.py
+++ arhan_ocr_task/script_images.py
@@ -2,30 +2,32 @@
 # how to run ? --> python script_images.py <folder_paht>
 # this is script is for ocr. a folder containing images will be converted to text and it will be stored in output.txt within that folder
 
-from pdf2image import convert_from_path
-from pytesseract import image_to_string
+#from pdf2image import convert_from_path
+import pytesseract
 import sys
 import os
+import cv2 as cv
 from PIL import Image
 
 # Path to the folder containing images
-folder_path = sys.argv[1]
+folder_path = "/Users/arhan.sheth/Documents/Codes/DX/Basics/dx_opencvInitial/arhan_ocr_task/images/sample_input_images" #sys.argv[1]
 
 # Prepare the path for the output text file within the same folder
 output_file_path = os.path.join(folder_path, 'output.txt')
 
 # List all image files in the directory assuming they are in JPG format. Modify the extension if different.
-image_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith('.jpg')]
+image_files = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith('.jpeg')]
 
 # Open the output file to write the extracted text
 with open(output_file_path, 'w') as file:
     # Iterate through each image file
     for image_path in image_files:
+        print(image_path)
         # Open the image file
         img = Image.open(image_path)
 
         # Extract text from image using pytesseract
-        text = image_to_string(img,config="-l guj") # changes it for using other languages
+        text = pytesseract.image_to_string(img,config="-l san") # changes it for using other languages
 
         # Write the extracted text to the output file
         file.write(text)
